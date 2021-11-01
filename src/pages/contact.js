ffb6d702b1d85758c08ef768e86de19fa3ce24aa
Aaron Durant <aaron@aarondurant.com>
2021-11-01 17:50:05 -0400

    Create first pages

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>My boss at my first job always told me he only hired me because I had my own email address.</p>
+      <p>With that critical piece of information now shared, I'd love to hear from you:</p>
+      <a href="mailto:aaron@aarondurant.com">aaron@aarondurant.com</a>
+      <p>Oh, and that Twitter thing: <a href="https://twitter.com/AaronRDurant" target="_blank">@AaronRDurant</a></p>
+    </div>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I'm Aaron, a web developer living in Michigan.</h2>
+      <p>Need a developer? <Link to="/contact">Get in touch</Link>.</p>
+    </div>
+  )
 }
+
+export default HomePage
